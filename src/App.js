62bb37a9f5fbf03ebf4a45f8fe4b9461028f1f53
antonkornilov-ua <anton.kornilov@hotmail.com>
2022-12-02 15:45:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [ count, setCount ] = useState(0)
+  
+  const incrementCount = () => {
+    setCount(count+1)
+  }
+  const decrementCount = () => {
+    setCount(count-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div>
+              <h2>Лічильник:</h2>
+              <h1>{count}</h1>
+              <button className="minus" onClick={decrementCount}>
+                  Відняти
+              </button>
+              <button className="plus" onClick={incrementCount}>
+                  Додати
+              </button>
+          </div>
+      </div>
   );
 }
 
